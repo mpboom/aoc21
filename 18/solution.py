48af030806839164a3c68b45d9f4c89cb8bcbeb0
Mark Boom <git@mpboom.com>
2021-12-20 15:14:26 +0100

    Day 18: Code improvements

--- 18/solution.py
+++ 18/solution.py
@@ -1,34 +1,33 @@
 import json
 import collections
-import math
 import itertools
 import copy
 
 
-def get_deep(data, idx, new_value=None):
+def deep_data(data, idx, new_value=None):
     idx = list(idx)
-    ptr = data
+    pointer = data
     while len(idx) - (1 if new_value != None else 0):
-        ptr = ptr[idx.pop(0)]
+        pointer = pointer[idx.pop(0)]
     if new_value != None:
-        ptr[idx.pop()] = new_value
-    return ptr if new_value is None else None
+        pointer[idx.pop()] = new_value
+    return pointer if new_value is None else None
 
 
 def build_tree(data):
     tree = collections.OrderedDict()
     stack = [0]
     while stack:
-        c = get_deep(data, stack)
+        c = deep_data(data, stack)
         is_pair = type(c) == list and len(c) == 2 and type(c[0]) == int and type(c[1]) == int
         if is_pair or type(c) == int:
             tree[tuple(stack)] = c
         if type(c) == list and not is_pair:
             stack.append(0)
-        elif len(get_deep(data, stack[:-1])) > stack[-1] + 1:
+        elif len(deep_data(data, stack[:-1])) > stack[-1] + 1:
             stack[-1] += 1
         else:
-            while stack and not len(get_deep(data, stack[:-1])) > stack[-1] + 1:
+            while stack and not len(deep_data(data, stack[:-1])) > stack[-1] + 1:
                 stack.pop()
             if stack:
                 stack[-1] += 1
@@ -36,67 +35,52 @@ def build_tree(data):
 
 
 def add(a, b):
-
     data = [a, b]
-    previous = None
-    while True:
-
+    data_delta = None
+    while str(data) != data_delta:
+        data_delta = str(data)
         tree = build_tree(data)
-        exploded = False
-
-        # Explode
-        prev_idx = None
-        iterator = iter(tree.items())
-        for idx, val in iterator:
+        explode_iterator = iter(tree.items())
+        previous_item = None
+        for idx, val in explode_iterator:  # explode
             if len(idx) == 4 and type(val) == list:
-                exploded = True
+                next_item = next(explode_iterator, None)
                 left, right = val
-                get_deep(data, idx, new_value=0)
-                if prev_idx:
-                    if type(prev_idx[1]) == list:
-                        prev_idx = (list(prev_idx[0]) + [1], prev_idx[1][1])
-                    get_deep(data, prev_idx[0], new_value=prev_idx[1] + left)
-                next_item = next(iterator, None)
+                deep_data(data, idx, new_value=0)
+                if previous_item:
+                    is_pair = type(previous_item[1]) == list
+                    deep_data(
+                        data,
+                        list(previous_item[0]) + ([1] if is_pair else []),
+                        new_value=(previous_item[1] if not is_pair else previous_item[1][1]) + left
+                    )
                 if next_item:
-                    if type(next_item[1]) == list:
-                        next_item = (list(next_item[0]) + [0], next_item[1][0])
-                    get_deep(data, next_item[0], new_value=next_item[1] + right)
+                    is_pair = type(next_item[1]) == list
+                    deep_data(
+                        data,
+                        list(next_item[0]) + ([0] if is_pair else []),
+                        new_value=(next_item[1] if not is_pair else next_item[1][0]) + right
+                    )
                 break
-            prev_idx = (idx, val)
-
-        if exploded:
-            continue
-
-        # Split
-        for idx, val in tree.items():
-            offender = None
-            if type(val) == int and val > 9:
-                offender = idx
-            elif type(val) == list and max(val) > 9:
-                offender = list(idx) + [0 if val[0] > 9 else 1]
-                val = val[0 if val[0] > 9 else 1]
-            if offender:
-                get_deep(data, offender, new_value=[math.floor(val / 2), math.ceil(val / 2)])
-                break
-
-        if str(data) == previous:
-            break
-        previous = str(data)
-
+            previous_item = (idx, val)
+        else:  # split
+            for idx, val in tree.items():
+                offender = idx if type(val) == int and val > 9 else None
+                if type(val) == list and max(val) > 9:
+                    offender = list(idx) + [0 if val[0] > 9 else 1]
+                    val = val[0 if val[0] > 9 else 1]
+                if offender:
+                    deep_data(data, offender, new_value=[val // 2, -(-val//2)])
+                    break
     return data
 
 
 def magnitude(data):
-    previous = None
-    while True:
+    while not (len(data) == 2 and type(data[0]) == int and type(data[1]) == int):
         result_tree = build_tree(data)
         for idx, item in result_tree.items():
             if type(item) != int:
-                get_deep(data, idx, new_value=3 * item[0] + 2 * item[1])
-        if str(data) == previous:
-            break
-        previous = str(data)
-    result_tree = build_tree(data)
+                deep_data(data, idx, new_value=3 * item[0] + 2 * item[1])
     return 3 * data[0] + 2 * data[1]
 
 
@@ -104,18 +88,12 @@ with open('input.txt') as fp:
     raw_data = [json.loads(x) for x in fp.read().split('\n') if len(x) > 2]
 
 # Part I
-data = copy.deepcopy(raw_data)
-result = data.pop(0)
-while data:
-    result = add(result, data.pop(0))
-print(magnitude(result))
+print(magnitude(
+    list(itertools.accumulate(copy.deepcopy(raw_data), lambda a, b: add(a, b) if a else b))[-1]
+))
 
 # Part II
-max_magnitude = 0
-for a, b in itertools.product(raw_data, raw_data):
-    max_magnitude = max(
-        magnitude(add(copy.deepcopy(a), copy.deepcopy(b))),
-        magnitude(add(copy.deepcopy(b), copy.deepcopy(a))),
-        max_magnitude
-    )
-print(max_magnitude)
+print(max(max(
+    magnitude(add(copy.deepcopy(a), copy.deepcopy(b))),
+    magnitude(add(copy.deepcopy(b), copy.deepcopy(a))),
+) for a, b in itertools.product(raw_data, raw_data)))
